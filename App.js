f74a8cf0631765f11ca014dbec2e7d959300fec6
fedeceb <federico-ceballos-1999@outlook.com>
2023-01-23 21:00:22 -0300

    prueba

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [task, setTask] = useState('');
+
+  const onHandlerChange = (text) => {
+    setTask(text)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput 
+          style={styles.input} 
+          placeholder='add a new task'
+          autoComplete='off'
+          autoCorrect={false}
+          autoCapitalize='none'
+          value={task}
+          onChangeText={onHandlerChange}
+        />
+        <Button title='Add' color='#70877f' />
+      </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  inputContainer: {
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    marginTop: 50,
+    marginHorizontal: 20,
+  },
+  input: {
+    width:'75%',
+    borderBottomColor: '#c4a77d',
+    borderBottomWidth: 1,
+    height: 40,
+    color: '#212121'
+  }
 });
